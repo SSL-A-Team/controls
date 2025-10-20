0c0d13189f99643a3c4efddb8bb5cea070d133b3
Nicholas Witten <nwitten@microsoft.com>
2025-10-19 22:24:38 -0700

    add robot model

--- ateam_controls/src/lib.rs
+++ ateam_controls/src/lib.rs
@@ -1,7 +1,8 @@
 pub mod trajectory_params;
 pub mod robot_physical_params;
 pub mod bang_bang_trajectory;
-pub mod wheel_conversion;
+pub mod robot_model;
+use nalgebra::{Vector3, Vector4};
 
 
 #[derive(Clone, Copy, Default, Debug)]
@@ -21,11 +22,103 @@ pub struct GlobalControl2Order {
     pub zdd: f64,
 }
 
+impl GlobalControl2Order {
+    pub fn from_vec(vec: &Vector3<f64>) -> Self {
+        Self {
+            xdd: vec[0],
+            ydd: vec[1],
+            zdd: vec[2],
+        }
+    }
+
+    pub fn to_vec(&self) -> Vector3<f64> {
+        Vector3::<f64>::new(
+            self.xdd,
+            self.ydd,
+            self.zdd,
+        )
+    }
+}
+
 #[derive(Clone, Copy, Default, Debug)]
 pub struct GlobalControl1Order {
     pub xd: f64,
     pub yd: f64,
     pub zd: f64,
+}
+
+impl GlobalControl1Order {
+    pub fn from_vec(vec: &Vector3<f64>) -> Self {
+        Self {
+            xd: vec[0],
+            yd: vec[1],
+            zd: vec[2],
+        }
+    }
+
+    pub fn to_vec(&self) -> Vector3<f64> {
+        Vector3::<f64>::new(
+            self.xd,
+            self.yd,
+            self.zd,
+        )
+    }
+}
+
+#[derive(Clone, Copy, Default, Debug)]
+pub struct WheelTorques {
+    pub torque_fl: f64,
+    pub torque_bl: f64,
+    pub torque_br: f64,
+    pub torque_fr: f64,
+}
+
+impl WheelTorques {
+    pub fn from_vec(vec: &Vector4<f64>) -> Self {
+        Self {
+            torque_fl: vec[0],
+            torque_bl: vec[1],
+            torque_br: vec[2],
+            torque_fr: vec[3],
+        }
+    }
+
+    pub fn to_vec(&self) -> Vector4<f64> {
+        Vector4::<f64>::new(
+            self.torque_fl,
+            self.torque_bl,
+            self.torque_br,
+            self.torque_fr,
+        )
+    }
+}
+
+#[derive(Clone, Copy, Default, Debug)]
+pub struct WheelVelocities {
+    pub velocity_fl: f64,
+    pub velocity_bl: f64,
+    pub velocity_br: f64,
+    pub velocity_fr: f64,
+}
+
+impl WheelVelocities {
+    pub fn from_vec(vec: &Vector4<f64>) -> Self {
+        Self {
+            velocity_fl: vec[0],
+            velocity_bl: vec[1],
+            velocity_br: vec[2],
+            velocity_fr: vec[3],
+        }
+    }
+
+    pub fn to_vec(&self) -> Vector4<f64> {
+        Vector4::<f64>::new(
+            self.velocity_fl,
+            self.velocity_bl,
+            self.velocity_br,
+            self.velocity_fr,
+        )
+    }
 }
 
 pub fn add(left: u64, right: u64) -> u64 {
---
+++ ateam_controls/src/robot_model.rs
@@ -0,0 +1,73 @@
+use crate::{GlobalState, WheelTorques, WheelVelocities, GlobalControl1Order, GlobalControl2Order};
+use nalgebra::{Matrix3, Matrix3x4, Matrix4x3, Vector3};
+
+
+struct RobotModel {
+    wheel_conversion_mat: Matrix3x4<f64>,
+    wheel_conversion_mat_inv: Matrix4x3<f64>,
+}
+
+impl RobotModel {
+    pub fn new_from_alpha_beta_l(alpha: f64, beta: f64, l: f64) -> RobotModel {
+        let mat = Matrix3x4::new(
+            -alpha.cos(), -beta.cos(),  beta.cos(), alpha.cos(),
+             alpha.sin(), -beta.sin(), -beta.sin(), alpha.sin(),
+             l          ,  l         ,  l         , l
+        );
+        let mat_inv = pinv_3x4(mat);
+        RobotModel {wheel_conversion_mat: mat, wheel_conversion_mat_inv: mat_inv}
+    }
+
+    // TODO update to global_control_1order_to_wheel_velocities
+    // z = 0 for local robot frame
+    pub fn global_state_to_wheel_velocities(&self, current_state: GlobalState) -> WheelVelocities {
+        let z = current_state.z;
+        let rotation_mat = Matrix3::<f64>::new(
+            z.cos(),-z.sin(), 0.0,
+            z.sin(), z.cos(), 0.0,
+            0.0    , 0.0    , 1.0,
+        );
+        let cartesian_velocities = Vector3::<f64>::new(
+            current_state.xd, current_state.yd, current_state.zd,
+        );
+        let wheel_velocities = (rotation_mat * self.wheel_conversion_mat).transpose() * cartesian_velocities;
+        WheelVelocities::from_vec(&wheel_velocities)
+    }
+
+    // z = 0 for local robot frame
+    pub fn wheel_velocities_to_global_control_1order(&self, wheel_velocities: WheelVelocities, z: f64) -> GlobalControl1Order {
+        todo!();
+    }
+
+    // z = 0 for local robot frame
+    pub fn wheel_torques_to_global_control_2order(&self, torques: WheelTorques, z: f64) -> GlobalControl2Order {
+        let rotation_mat = Matrix3::<f64>::new(
+            z.cos(),-z.sin(), 0.0,
+            z.sin(), z.cos(), 0.0,
+            0.0    , 0.0    , 1.0,
+        );
+        let wheel_torques = torques.to_vec();
+        let accelerations = rotation_mat * self.wheel_conversion_mat * wheel_torques;
+        GlobalControl2Order::from_vec(&accelerations)
+    }
+
+    // z = 0 for local robot frame
+    pub fn global_control_2order_to_wheel_torques(&self, global_control: GlobalControl2Order, z: f64) -> WheelTorques {
+        let rotation_mat = Matrix3::<f64>::new(
+            (-z).cos(), z.sin()   , 0.0,
+            (-z).sin(), (-z).cos(), 0.0,
+            0.0       , 0.0       , 1.0,
+        );
+        let control = global_control.to_vec();
+        let torques = self.wheel_conversion_mat_inv * rotation_mat * control;
+        WheelTorques::from_vec(&torques)
+    }
+}
+
+fn pinv_3x4(a: Matrix3x4<f64>) -> Matrix4x3<f64> {
+    let a_t = a.clone().transpose();
+    let a_at = a * a_t.clone();
+    let a_at_inv = a_at.try_inverse().unwrap();
+    let a_inv = a_t * a_at_inv;
+    a_inv
+}
