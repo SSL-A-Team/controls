fd38384b88da545a11de5e0715f9a366c718c148
Nicholas Witten <nwitt12@gmail.com>
2025-12-23 11:45:20 -0500

    bangbang trajectory rust implementation

--- ateam-controls/src/lib.rs
+++ ateam-controls/src/lib.rs
@@ -15,6 +15,13 @@ pub type Matrix4x3f = nalgebra::Matrix4x3<f32>;
 pub type Matrix3f = nalgebra::Matrix3<f32>;
 pub type Rotation3f = nalgebra::Rotation3<f32>;
 
+
+#[derive(Clone, Copy, Default, Debug)]
+pub struct RigidBodyState {
+    pub pose: Vector3f,
+    pub twist: Vector3f,
+}
+
 pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
