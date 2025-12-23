f29c0ff0af4789aa4bf7aece97350da928640222
Nicholas Witten <nwitt12@gmail.com>
2025-12-23 11:48:57 -0500

    c bindings and ctests

--- ateam-controls-c/src/lib.rs
+++ ateam-controls-c/src/lib.rs
@@ -1,6 +1,100 @@
 use ateam_controls;
+use ateam_controls::ctypes::*;
+use ateam_controls::bangbang_trajectory::*;
+use ateam_controls::robot_model::*;
+
 
 #[unsafe(no_mangle)]
 pub extern "C" fn ateam_controls_add(left: u64, right: u64) -> u64 {
   ateam_controls::add(left, right)
+}
+
+#[unsafe(no_mangle)]
+pub extern "C" fn ateam_controls_transform_frame_global2robot_pose(robot_pose: Vector3C, pose: Vector3C) -> Vector3C {
+  transform_frame_global2robot_pose(robot_pose.into(), pose.into()).into()
+}
+
+#[unsafe(no_mangle)]
+pub extern "C" fn ateam_controls_transform_frame_robot2global_pose(robot_pose: Vector3C, pose: Vector3C) -> Vector3C {
+  transform_frame_robot2global_pose(robot_pose.into(), pose.into()).into()
+}
+
+#[unsafe(no_mangle)]
+pub extern "C" fn ateam_controls_transform_frame_global2robot_twist(robot_pose: Vector3C, twist: Vector3C) -> Vector3C {
+  transform_frame_global2robot_twist(robot_pose.into(), twist.into()).into()
+}
+
+#[unsafe(no_mangle)]
+pub extern "C" fn ateam_controls_transform_frame_robot2global_twist(robot_pose: Vector3C, twist: Vector3C) -> Vector3C {
+  transform_frame_robot2global_twist(robot_pose.into(), twist.into()).into()
+}
+
+#[unsafe(no_mangle)]
+pub extern "C" fn ateam_controls_transform_frame_global2robot_accel(robot_pose: Vector3C, accel: Vector3C) -> Vector3C {
+  transform_frame_global2robot_accel(robot_pose.into(), accel.into()).into()
+}
+
+#[unsafe(no_mangle)]
+pub extern "C" fn ateam_controls_transform_frame_robot2global_accel(robot_pose: Vector3C, accel: Vector3C) -> Vector3C {
+  transform_frame_robot2global_accel(robot_pose.into(), accel.into()).into()
+}
+
+#[unsafe(no_mangle)]
+pub extern "C" fn ateam_controls_robot_model_new_from_constants() -> RobotModelC {
+  RobotModel::new_from_constants().into()
+}
+
+#[unsafe(no_mangle)]
+pub extern "C" fn ateam_controls_robot_model_wheel_velocities_to_twist(robot_model: RobotModelC, wheel_velocities: Vector4C) -> Vector3C {
+  RobotModel::from(robot_model).wheel_velocities_to_twist(wheel_velocities.into()).into()
+}
+
+#[unsafe(no_mangle)]
+pub extern "C" fn ateam_controls_robot_model_twist_to_wheel_velocities(robot_model: RobotModelC, twist: Vector3C) -> Vector4C {
+  RobotModel::from(robot_model).twist_to_wheel_velocities(twist.into()).into()
+}
+
+#[unsafe(no_mangle)]
+pub extern "C" fn ateam_controls_robot_model_wheel_torques_to_accel(robot_model: RobotModelC, torques: Vector4C) -> Vector3C {
+  RobotModel::from(robot_model).wheel_torques_to_accel(torques.into()).into()
+}
+
+#[unsafe(no_mangle)]
+pub extern "C" fn ateam_controls_robot_model_accel_to_wheel_torques(robot_model: RobotModelC, accel: Vector3C) -> Vector4C {
+  RobotModel::from(robot_model).accel_to_wheel_torques(accel.into()).into()
+}
+
+#[unsafe(no_mangle)]
+pub extern "C" fn ateam_controls_bangbang_traj_1d_time_shift(traj: *mut BangBangTraj1D, dt: f32) {
+  unsafe {(&mut *traj).time_shift(dt)};
+}
+
+#[unsafe(no_mangle)]
+pub extern "C" fn ateam_controls_bangbang_traj_3d_time_shift(traj: *mut BangBangTraj3D, dt: f32) {
+  unsafe {(&mut *traj).time_shift(dt)};
+}
+
+#[unsafe(no_mangle)]
+pub extern "C" fn ateam_controls_bangbang_traj_3d_get_end_time(traj: BangBangTraj3D) -> f32 {
+  traj.get_end_time()
+}
+
+#[unsafe(no_mangle)]
+pub extern "C" fn ateam_controls_compute_optimal_bangbang_traj_3d(init_state: RigidBodyStateC, target_pose: Vector3C) -> BangBangTraj3D {
+  compute_optimal_bangbang_traj_3d(init_state.into(), target_pose.into())
+}
+
+#[unsafe(no_mangle)]
+pub extern "C" fn ateam_controls_compute_bangbang_traj_3d_state_at_t(traj: BangBangTraj3D, current_state: RigidBodyStateC, current_time: f32, t: f32) -> RigidBodyStateC {
+  compute_bangbang_traj_3d_state_at_t(traj, current_state.into(), current_time, t).into()
+}
+
+#[unsafe(no_mangle)]
+pub extern "C" fn ateam_controls_compute_bangbang_traj_3d_accel_at_t(traj: BangBangTraj3D, t: f32) -> Vector3C {
+  compute_bangbang_traj_3d_accel_at_t(traj, t).into()
+}
+
+#[unsafe(no_mangle)]
+pub extern "C" fn ateam_controls_compute_bangbang_traj_1d_accel_at_t(traj: BangBangTraj1D, t: f32) -> f32 {
+  compute_bangbang_traj_1d_accel_at_t(traj, t)
 }
---
+++ ateam-controls/src/ctypes.rs
@@ -0,0 +1,223 @@
+use nalgebra::{Vector3, Vector4, Matrix3, Matrix3x4, Matrix4x3};
+use crate::{RigidBodyState, robot_model::RobotModel};
+
+#[repr(C)]
+#[derive(Clone, Copy, Default, Debug)]
+pub struct Vector3C {
+    pub x: f32,
+    pub y: f32,
+    pub z: f32,
+}
+
+impl From<Vector3<f32>> for Vector3C {
+    fn from(v: Vector3<f32>) -> Self {
+        Vector3C {x: v.x, y: v.y, z: v.z}
+    }
+}
+
+impl From<Vector3C> for Vector3<f32> {
+    fn from(v: Vector3C) -> Self {
+        Self::new(v.x, v.y, v.z)
+    }
+}
+
+#[repr(C)]
+#[derive(Clone, Copy, Default, Debug)]
+pub struct Vector4C {
+    pub x: f32,
+    pub y: f32,
+    pub z: f32,
+    pub w: f32,
+}
+
+impl From<Vector4<f32>> for Vector4C {
+    fn from(v: Vector4<f32>) -> Self {
+        Vector4C {x: v.x, y: v.y, z: v.z, w: v.w}
+    }
+}
+
+impl From<Vector4C> for Vector4<f32> {
+    fn from(v: Vector4C) -> Self {
+        Self::new(v.x, v.y, v.z, v.w)
+    }
+}
+
+#[repr(C)]
+#[derive(Clone, Copy, Default, Debug)]
+pub struct Matrix3C {
+    pub data: [f32; 9],
+}
+
+impl Matrix3C {
+    /// Create a new Matrix3C from a flat array in column-major order
+    pub fn new(data: [f32; 9]) -> Self {
+        Matrix3C { data }
+    }
+
+    pub fn get(&self, row: usize, col: usize) -> f32 {
+        self.data[col * 3 + row]
+    }
+
+    pub fn set(&mut self, row: usize, col: usize, value: f32) {
+        self.data[col * 3 + row] = value;
+    }
+}
+
+impl From<Matrix3<f32>> for Matrix3C {
+    fn from(mat: Matrix3<f32>) -> Self {
+        let mut data = [0.0f32; 9];
+        for i in 0..3 {
+            for j in 0..3 {
+                data[j * 3 + i] = mat[(i, j)];
+            }
+        }
+        Matrix3C { data }
+    }
+}
+
+impl From<Matrix3C> for Matrix3<f32> {
+    fn from(mat: Matrix3C) -> Self {
+        Matrix3::from_column_slice(&mat.data)
+    }
+}
+
+
+#[repr(C)]
+#[derive(Clone, Copy, Default, Debug)]
+pub struct Matrix3x4C {
+    pub data: [f32; 12],
+}
+
+impl Matrix3x4C {
+    /// Create a new Matrix3x4C from a flat array in column-major order
+    pub fn new(data: [f32; 12]) -> Self {
+        Matrix3x4C { data }
+    }
+
+    pub fn get(&self, row: usize, col: usize) -> f32 {
+        // 3 rows, 4 cols
+        self.data[col * 3 + row]
+    }
+
+    pub fn set(&mut self, row: usize, col: usize, value: f32) {
+        self.data[col * 3 + row] = value;
+    }
+}
+
+impl From<Matrix3x4<f32>> for Matrix3x4C {
+    fn from(mat: Matrix3x4<f32>) -> Self {
+        let mut data = [0.0f32; 12];
+        for i in 0..3 {
+            for j in 0..4 {
+                data[j * 3 + i] = mat[(i, j)];
+            }
+        }
+        Matrix3x4C { data }
+    }
+}
+
+impl From<Matrix3x4C> for Matrix3x4<f32> {
+    fn from(mat: Matrix3x4C) -> Self {
+        Matrix3x4::from_column_slice(&mat.data)
+    }
+}
+
+
+#[repr(C)]
+#[derive(Clone, Copy, Default, Debug)]
+pub struct Matrix4x3C {
+    pub data: [f32; 12],
+}
+
+impl Matrix4x3C {
+    /// Create a new Matrix4x3C from a flat array in column-major order
+    pub fn new(data: [f32; 12]) -> Self {
+        Matrix4x3C { data }
+    }
+
+    pub fn get(&self, row: usize, col: usize) -> f32 {
+        // 4 rows, 3 cols
+        self.data[col * 4 + row]
+    }
+
+    pub fn set(&mut self, row: usize, col: usize, value: f32) {
+        self.data[col * 4 + row] = value;
+    }
+}
+
+impl From<Matrix4x3<f32>> for Matrix4x3C {
+    fn from(mat: Matrix4x3<f32>) -> Self {
+        let mut data = [0.0f32; 12];
+        for i in 0..4 {
+            for j in 0..3 {
+                data[j * 4 + i] = mat[(i, j)];
+            }
+        }
+        Matrix4x3C { data }
+    }
+}
+
+impl From<Matrix4x3C> for Matrix4x3<f32> {
+    fn from(mat: Matrix4x3C) -> Self {
+        Matrix4x3::from_column_slice(&mat.data)
+    }
+}
+
+#[repr(C)]
+#[derive(Clone, Copy, Default, Debug)]
+pub struct RigidBodyStateC {
+    pub pose: Vector3C,
+    pub twist: Vector3C,
+}
+
+impl From<RigidBodyState> for RigidBodyStateC {
+    fn from(state: RigidBodyState) -> Self {
+        Self {
+            pose: state.pose.into(),
+            twist: state.twist.into(),
+        }
+    }
+}
+
+impl From<RigidBodyStateC> for RigidBodyState {
+    fn from(state: RigidBodyStateC) -> Self {
+        Self {
+            pose: state.pose.into(),
+            twist: state.twist.into(),
+        }
+    }
+}
+
+#[repr(C)]
+#[derive(Clone, Copy, Default, Debug)]
+pub struct RobotModelC {
+    wheel_transform_mat: Matrix3x4C,
+    wheel_transform_mat_inv: Matrix4x3C,
+    wheel_radius: f32,
+    body_inirtia: Matrix3C,
+    body_inirtia_inv: Matrix3C,
+}
+
+impl From<RobotModel> for RobotModelC {
+    fn from(robot_model: RobotModel) -> Self {
+        Self {
+            wheel_transform_mat: robot_model.wheel_transform_mat.into(),
+            wheel_transform_mat_inv: robot_model.wheel_transform_mat_inv.into(),
+            wheel_radius: robot_model.wheel_radius.into(),
+            body_inirtia: robot_model.body_inirtia.into(),
+            body_inirtia_inv: robot_model.body_inirtia_inv.into(),
+        }
+    }
+}
+
+impl From<RobotModelC> for RobotModel {
+    fn from(robot_model: RobotModelC) -> Self {
+        Self {
+            wheel_transform_mat: robot_model.wheel_transform_mat.into(),
+            wheel_transform_mat_inv: robot_model.wheel_transform_mat_inv.into(),
+            wheel_radius: robot_model.wheel_radius.into(),
+            body_inirtia: robot_model.body_inirtia.into(),
+            body_inirtia_inv: robot_model.body_inirtia_inv.into(),
+        }
+    }
+}
