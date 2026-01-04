d7476804f9e8a1330e429516b45e92358ae20e2d
Nicholas Witten <nwitt12@gmail.com>
2026-01-03 17:57:40 -0800

    update params

--- ateam-controls/src/trajectory_params.rs
+++ ateam-controls/src/trajectory_params.rs
@@ -3,7 +3,7 @@ pub use core::f32::consts::PI;
 
 pub const ALLOWABLE_ERROR_POS: f32 = 0.005;  // m
 pub const ALLOWABLE_ERROR_VEL: f32 = 0.005;  // m/s
-pub const MAX_TRANSLATIONAL_ACCELERATION: f32 = 1.0;  // m/s^2
-pub const MAX_TRANSLATIONAL_VELOCITY: f32 = 1.0;  // m/s
-pub const MAX_ROTATIONAL_ACCELERATION: f32 = 2.0 * PI;  // rad/s^2
-pub const MAX_ROTATIONAL_VELOCITY: f32 = 2.0 * PI;  // rad/s
+pub const MAX_TRANSLATIONAL_ACCELERATION: f32 = 5.0;  // m/s^2
+pub const MAX_TRANSLATIONAL_VELOCITY: f32 = 2.0;  // m/s
+pub const MAX_ROTATIONAL_ACCELERATION: f32 = 5.0 * 2.0 * PI;  // rad/s^2
+pub const MAX_ROTATIONAL_VELOCITY: f32 = 2.0 * 2.0 * PI;  // rad/s
