c1112e64ea2fb9e2a8ea5fe5750e3cd1e91d170e
Nicholas Witten <nwitt12@gmail.com>
2026-05-05 16:56:10 -0700

    set max accel linear param to 2.0

--- ateam-controls/src/trajectory_params.rs
+++ ateam-controls/src/trajectory_params.rs
@@ -3,5 +3,5 @@ pub use core::f32::consts::PI;
 
 pub const MAX_VEL_LINEAR: f32 = 3.0;  // m/s
 pub const MAX_VEL_ANGULAR: f32 = 10.0;  // rad/s
-pub const MAX_ACCEL_LINEAR: f32 = 1.0;  // m/s^2
+pub const MAX_ACCEL_LINEAR: f32 = 2.0;  // m/s^2
 pub const MAX_ACCEL_ANGULAR: f32 = 6.283;  // rad/s^2
